7c2f7e23d0ba5422d22db0745f54d8cfb3ee555c
Damian Yates <dyates@damianjyates.com>
2021-05-13 09:54:13 -0700

    Commiting Changes

--- Darigold/Add-BulkADGroups.ps1
+++ Darigold/Add-BulkADGroups.ps1
@@ -1,5 +1,6 @@
 <#
 .SYNOPSIS
+    *Script is broken* 
     This script collects input information from user needed to properly create bulk Active Directory Security Groups 
 .DESCRIPTION
     Information Pending
@@ -14,18 +15,18 @@
 Import-Module ActiveDirectory
 
 #Ask for the path to the csv file for processing
-$csv = Read-Host -Prompt "Enter location of csv"
+#$csv = Read-Host -Prompt "Enter location of csv"
 
 #Ask for the DN of the container to create the groups in
-$OU = Read-Host -Prompt "Enter the DN of the container to place the new groups in"
+#$OU = Read-Host -Prompt "Enter the DN of the container to place the new groups in"
 
 #Ask for the Owner of the group. *Note: Enter the "Display Name" of the regular account of the user.
-$owner = Read-Host -Prompt "Enter the name of the Requestor of the Group"
+#$owner = Read-Host -Prompt "Enter the name of the Requestor of the Group"
 
 #Begin Script Processing
 
-$groups = Import-Csv $csv #Import list of groups from csv file
-$manager = Get-ADUser -Filter "Name -eq $owner" | Select samaccountname #Collect samaccountname of Owner
+$groups = Import-Csv "C:\Users\dayates\OneDrive - Darigold\Documents\Draft Documents\Ticket Work\0045913\Groups.csv" #Import list of groups from csv file
+$manager = Get-ADUser -Filter "Name -eq '$owner'" | Select samaccountname #Collect samaccountname of Owner
 foreach ($group in $groups) {
-New-ADGroup -Name $group.name -Path $OU -Description “New Groups Created in Bulk” -GroupCategory Security -GroupScope Universal -Managedby $manager #Create a group for each item in csv
+New-ADGroup -Name $group.name -Path "OU=Controls Groups,OU=Groups,OU=Darigold Users,DC=westfarm,DC=com" -Description “New Groups Created in Bulk” -GroupCategory Security -GroupScope Global -Managedby "Brian Ackerson" #Create a group for each item in csv
 }
---
+++ Darigold/Check_StaleDNS.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+    This script checks primary DNS server for stale records, test connectivity, and report status of record
+.DESCRIPTION
+    Pending
+.NOTES
+    File Name: Check_StaleDNS.ps1
+    Author: Damian J. Yates
+    Built on Powershell Version: 
+    Created Date: 05/12/2021 
+    GIT REPO: https://github.com/damianjyates/Powershell
+#>
